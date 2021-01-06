35dab12c5f4c280e4183cd0f7511828877bb14e6
Patrick Palsbröker <patrick.palsbroeker@fh-bielefeld.de>
2021-01-06 15:37:10 +0100

    Added Example für AR + GPS Framework

---
+++ Assets/XR/Loaders/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoARGPSScene()
+    {
+        SceneManager.LoadScene("ARGPS Example");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("DevUI");
+    }
+}
