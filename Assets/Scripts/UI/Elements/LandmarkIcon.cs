b6ffa5564c1d32b357c0914f276173fd02357598
Marcos Baez <mbaezpy@gmail.com>
2022-09-07 12:13:58 +0200

    Created a reusable landmark icon prefab

---
+++ Assets/Scripts/UI/Elements/LandmarkIcon.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LandmarkIcon : MonoBehaviour
+{
+    public enum LandmarkType {
+        Placeholder = -1,
+        Train = 1,
+        Coffee = 2,
+        Work = 3,
+        Home = 4
+    }
+    public LandmarkType selectedLandmarkType = LandmarkType.Placeholder;
+    private LandmarkType activeLandmarkType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayLandmarkType(activeLandmarkType);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If a new selected landmark has been set, we activate it
+        if (activeLandmarkType != selectedLandmarkType)
+        {
+            displayLandmarkType(selectedLandmarkType);
+        }
+
+    }
+
+    // activates the selected landmark type
+    void displayLandmarkType(LandmarkType selected)
+    {
+        Debug.Log(gameObject.transform.childCount);
+        for (int i=0; i< gameObject.transform.childCount; i++)
+        {
+            Transform ch = gameObject.transform.GetChild(i);
+            Debug.Log(ch.name);
+
+            ch.gameObject.SetActive(ch.name == selected.ToString());
+            Debug.Log(ch.name + "--" + selected.ToString() + "=== " + (ch.name == selected.ToString()));
+
+        }
+
+        activeLandmarkType = selected;
+    }
+}
